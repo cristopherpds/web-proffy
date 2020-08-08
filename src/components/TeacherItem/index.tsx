ffa4431a078f6782958939058c2bc1f7c1fee2e7
Cristopher Paiva <josuepaiva7@gmail.com>
2020-08-08 14:34:11 -0300

    loading connections in the landing
    registration of new classes
    
    teacher listing

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -4,33 +4,43 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css'
 
+export interface Teacher {
 
-function TeacherItem(){
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: number;
+}
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+
+const TeacherItem: React.FC<TeacherItemProps> = ({teacher}) => {
     return(
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars3.githubusercontent.com/u/29117443?s=460&u=d355fe3eff687fded7559a0e16568c3722c21a7b&v=4" alt="Cristopher Paiva"/>
+                        <img src={teacher.avatar} alt={teacher.name} />
                         <div>
                             <strong>
-                                Cristopher Paiva
-                                <span> Químico </span>
+                                {teacher.name}
+                                <span> {teacher.subject} </span>
                             </strong>
                         </div>
                     </header>
-                    <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br/> <br/>
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+                    <p>{teacher.bio} </p>
                     <footer>
                         <p>
                             Preço/hora 
-                            <strong>R$ 50,00</strong>
+                            <strong>R$ {teacher.cost} </strong>
                         </p>
-                        <button type="button">
+                        <a href={`https://wa.me/${teacher.whatsapp}`}>
                             <img src={whatsappIcon} alt="Whatsapp"/>
                             Entrar en contato
-                        </button>
+                        </a>
                     </footer>
                 </article>
     );
